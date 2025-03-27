1c94da45013a978a087b48a2f69238fc1a113a71
htl-leonding <t.stuetz@htl-leonding.ac.at>
2025-03-27 11:03:40 +0100

    2025-02-27 lecture notes mqtt lab

--- labs/angular-curriculum/vehicle-rental/src/app/app.component.ts
+++ labs/angular-curriculum/vehicle-rental/src/app/app.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { VehicleComponent } from './vehicle/vehicle.component';
+import { DUMMY_VEHICLES } from './dummy-vehicles';
+import { RentalsComponent } from './rentals/rentals.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
-    VehicleComponent
+    VehicleComponent,
+    RentalsComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'vehicle-rental';
+  vehicles = DUMMY_VEHICLES
+  selectedVehicleId = 'v1'
+
+  get selectedVehicle() {
+    return this.vehicles.find(vehicle => vehicle.id === this.selectedVehicleId)!
+  }
+
+  onSelectVehicle(id: string) {
+    this.selectedVehicleId = id;
+  }
 }
--- labs/angular-curriculum/vehicle-rental/src/app/vehicle/vehicle.component.ts
+++ labs/angular-curriculum/vehicle-rental/src/app/vehicle/vehicle.component.ts
@@ -1,5 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_VEHICLES} from '../dummy-vehicles';
+import { Component, computed, EventEmitter, input, Input, output, Output, signal } from '@angular/core';
+import { DUMMY_VEHICLES } from '../dummy-vehicles';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_VEHICLES.length);
 
@@ -10,15 +10,23 @@ const randomIndex = Math.floor(Math.random() * DUMMY_VEHICLES.length);
   styleUrl: './vehicle.component.css'
 })
 export class VehicleComponent {
-  selectedVehicle = signal(DUMMY_VEHICLES[randomIndex])
-  imagePath = computed(() => 'icons/' + this.selectedVehicle().avatar)
 
-  // get imagePath() {
-  //   return 'icons/' + this.selectedVehicle.avatar
-  // }
+  @Input({ required: true }) id!: string
+  @Input({ required: true }) avatar!: string
+  @Input({ required: true }) brand!: string
+  //@Output() select  = new EventEmitter<string>()
+  select = output<string>()
+
+  // avatar = input.required<string>()
+  // brand = input.required<string>()
+
+  get imagePath() {
+    return 'icons/' + this.avatar
+  }
+
+  // imagePath = computed(() => 'icons/' + this.avatar())
 
   onSelectVehicle() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_VEHICLES.length);
-    this.selectedVehicle.set( DUMMY_VEHICLES[randomIndex])
+    this.select.emit(this.id)
   }
 }
